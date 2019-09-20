a0cae92e1376857614ad30d60e68f8c45137f4ec
Juan Ruano <aplicacionesjuanr@gmail.com>
2019-09-20 12:54:38 +0200

    Add files via upload

---
+++ Movimiento.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Movimiento Coche Principal clase Movimiento.
+public class Movimiento : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	[SerializeField]
+	float accelerationPower = 5f;
+	[SerializeField]
+	float steeringPower = 5f;
+	float steeringAmount, speed, direction;
+
+	// Inicializar
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update, una vez por frame crea el movimiento 
+	void FixedUpdate () {
+
+		steeringAmount = - Input.GetAxis ("Horizontal");
+		speed = Input.GetAxis ("Vertical") * accelerationPower;
+		direction = Mathf.Sign(Vector2.Dot (rb.velocity, rb.GetRelativeVector(Vector2.up)));
+		rb.rotation += steeringAmount * steeringPower * rb.velocity.magnitude * direction;
+
+		rb.AddRelativeForce (Vector2.up * speed);
+
+		rb.AddRelativeForce ( - Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+			
+	}
+
+
+}
